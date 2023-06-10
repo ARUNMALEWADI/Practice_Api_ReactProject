dcd6f5a4a69940e0bc5990fa2f0fd425d5f5cb6a
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-10 16:49:33 +0530

    added loading ... to the fapplication

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies,setMovies]=useState([])
+  const [movies,setMovies]=useState([]);
+  const [IsLoading,SetLoading]=useState(false);
  async function FetchMovieHandler(){
+  console.log('Inside');
+  SetLoading(true)
+  console.log('afterloading');
   const response =await fetch('https://swapi.dev/api/films/')
- const data=await response.json()
+ const data=await response.json();
   const transfomeddata=data.results.map((movie)=>{return {id:movie.episode_id,
                          title:movie.title,
                          openingText:movie.opening_crawl,
                         releaseDate:movie.release_date}} )
-                        
+
                         setMovies(transfomeddata);
+                        console.log('aftersetmovies');
+                        SetLoading(false)
+                        console.log('afternot loading');
 
 }
+console.log('outside');
 
   return (
     <React.Fragment>
@@ -23,7 +31,8 @@ function App() {
         <button onClick={FetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       { !IsLoading && <MoviesList movies={movies} />}
+       {IsLoading&&<p>Loading.....</p>}
       </section>
     </React.Fragment>
   );
