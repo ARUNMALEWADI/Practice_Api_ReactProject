9cbad772c88b83527b3fd33dff5f1e610338eec8
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-10 15:50:57 +0530

    fetch using async wait

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const [movies,setMovies]=useState([])
+ async function FetchMovieHandler(){
+  const response =await fetch('https://swapi.dev/api/films/')
+ const data=await response.json()
+  const transfomeddata=data.results.map((movie)=>{return {id:movie.episode_id,
+                         title:movie.title,
+                         openingText:movie.opening_crawl,
+                        releaseDate:movie.release_date}} )
+                        
+                        setMovies(transfomeddata);
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={FetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
