bae83e822403c8d33ddbf143dd89b0bc04a0a996
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-11 10:47:12 +0530

    Added retry Handler

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import React, { useState } from 'react';
-
+import { useEffect } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies,setMovies]=useState([]);
   const [IsLoading,SetLoading]=useState(false);
- async function FetchMovieHandler(){
-  console.log('Inside');
-  SetLoading(true)
-  console.log('afterloading');
-  const response =await fetch('https://swapi.dev/api/films/')
- const data=await response.json();
-  const transfomeddata=data.results.map((movie)=>{return {id:movie.episode_id,
-                         title:movie.title,
-                         openingText:movie.opening_crawl,
-                        releaseDate:movie.release_date}} )
+  const [error,setError]=useState(null)
 
-                        setMovies(transfomeddata);
-                        console.log('aftersetmovies');
-                        SetLoading(false)
-                        console.log('afternot loading');
+
+ async function FetchMovieHandler(){
+  setError(null)
+  SetLoading(true)
+  try {
+   const response =await fetch('https://swapi.dev/api/film/')
+   if(!response.ok)
+    {        
+      throw new Error('Something went Wrong......');
+    
+    }
+   const data=await response.json();
+   
+    const transfomeddata=data.results.map((movie)=>{return {id:movie.episode_id,
+                           title:movie.title,
+                           openingText:movie.opening_crawl,
+                          releaseDate:movie.release_date}} )
+  
+                          setMovies(transfomeddata);
+                      
+                        
+                        
+                          
+    
+  } catch (error) { 
+   setError(error.message)
+ 
+    
+  }
+  SetLoading(false)
 
 }
-console.log('outside');
+
+const RetryingHandler=()=>{
+ 
+FetchMovieHandler();
+  
+  
+}
+
+
+
 
   return (
     <React.Fragment>
+     { !IsLoading && error && <button onClick={RetryingHandler} >Retry</button>}
       <section>
         <button onClick={FetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-       { !IsLoading && <MoviesList movies={movies} />}
+       { !IsLoading && movies.length>0&&<MoviesList movies={movies} />}
+       {!IsLoading&&movies.length==0 && <p>No movies </p>}
        {IsLoading&&<p>Loading.....</p>}
+       { !IsLoading && error &&<p>{error}</p>}
+    
       </section>
     </React.Fragment>
   );
